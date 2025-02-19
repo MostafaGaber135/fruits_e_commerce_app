1cfc9b07d2350ec873d94aa5eeebfdf4f17daea8
MostafaGaber135 <mostafagaber1234560@gmail.com>
2025-02-19 20:04:58 +0200

    finish feature on_boarding

--- lib/features/on_boarding/presentation/views/on_boarding_view.dart
+++ lib/features/on_boarding/presentation/views/on_boarding_view.dart
@@ -1,10 +1,16 @@
 import 'package:flutter/material.dart';
 
+import 'widgets/on_boarding_view_body.dart';
+
 class OnBoardingView extends StatelessWidget {
   const OnBoardingView({super.key});
   static const String routeName = 'onBoardingView';
   @override
   Widget build(BuildContext context) {
-    return Placeholder();
+    return Scaffold(
+      body: SafeArea(
+        child: OnBoardingViewBody(),
+      ),
+    );
   }
 }
